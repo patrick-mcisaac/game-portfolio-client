f972b393cd424b0cbbcf32842db857a2149c2297
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2026-01-19 13:55:17 -0600

    made home screen centered

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Logo from "@/components/logo/Logo"
 import dev_logo from "../public/svg/dev_logo.svg"
 
-export default function About() {
+export default function Home() {
     return (
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex h-screen flex-col items-center justify-center">
             <Logo svg={dev_logo} className="" width={1000} />
         </div>
     )
