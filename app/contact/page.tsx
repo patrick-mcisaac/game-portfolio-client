a78dbb34ea62e5c7a80400e86ab2ee4af8e29469
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2026-01-19 15:54:28 -0600

    all links work except email service

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,19 +1,12 @@
-// "use client"
 import ContactCard from "@/components/contact/ContactCard"
-import { getDevContact } from "@/data/contactRequest"
 import {
     faGithub,
     faLinkedin,
     faUnity
 } from "@fortawesome/free-brands-svg-icons"
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
-import { useQuery } from "@tanstack/react-query"
 
 export default function Page() {
-    // const { data, isSuccess } = useQuery({
-    //     queryKey: ["contact"],
-    //     queryFn: getDevContact
-    // })
     return (
         <div className="m-h-screen flex w-full flex-col items-center justify-start gap-40 self-center">
             <h1 className="text-3xl md:text-5xl lg:text-6xl xl:text-7xl">
--- components/contact/ContactCard.tsx
+++ components/contact/ContactCard.tsx
@@ -3,6 +3,7 @@ import { getDevContact } from "@/data/contactRequest"
 import { IconProp } from "@fortawesome/fontawesome-svg-core"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useQuery } from "@tanstack/react-query"
+import Link from "next/link"
 import React from "react"
 type ContactCardProps = {
     icon: IconProp
@@ -21,16 +22,22 @@ export default function ContactCard({
         queryFn: () => getDevContact(name),
         staleTime: 5 * 60 * 100
     })
-    if (isSuccess)
+    if (isSuccess && name !== "email")
         return (
-            <a
-                href={data.name == "email" ? `mailto:${data.link}` : data.link}
-                target="_blank"
-            >
+            <a href={data.link} target="_blank">
                 <FontAwesomeIcon
                     icon={icon}
                     className={`${className} text-3xl transition hover:scale-120 md:text-5xl lg:text-6xl xl:text-7xl`}
                 />
             </a>
+        )
+    if (isSuccess && name === "email")
+        return (
+            <Link href={"/"}>
+                <FontAwesomeIcon
+                    icon={icon}
+                    className={`${className} text-3xl transition hover:scale-120 md:text-5xl lg:text-6xl xl:text-7xl`}
+                />
+            </Link>
         )
 }
